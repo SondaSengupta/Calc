310f13b33951026becd314dcd97a826a26e996b1
Leon Peck <leonpeck234@gmail.com>
2014-11-07 15:51:28 -0600

    Moving onto subtraction.  WIP: Floating Points

--- main.js
+++ main.js
@@ -1,13 +1,56 @@
-  function displayOutput(){
-    return $('#displayoutput').val();
-  }
+function displayOutput(){
+  return $('#displayoutput').val();
+}
 
-  function press(buttonValue){
-    if ( ["+", "-", "*", "/", "=", "C", "+/-"].indexOf(buttonValue) !== -1 ) {
-      // handle operators
-    } else {
-    var current = $('#displayoutput').val();
-    $('#displayoutput').val(current += buttonValue);
-    }
-    
+var prior = 0;
+
+function press(buttonValue){
+
+  switch (buttonValue) {
+    case '+':
+      prior += +$('#displayoutput').val();
+
+      // NEED TO DEAL WITH FLOATING POINT IMPRECISION
+      // prior = (((prior * 10) + (+$('#displayoutput').val() * 10)) / 10);
+      // ((prior * 10) + (+$('#displayoutput').val() * 10)) / 10;
+      $('#displayoutput').val('');
+      break;
+
+    case '-':
+      // handle -
+      prior = prior - +$('#displayoutput').val();
+      $('#displayoutput').val('');
+      break;
+
+    case '*':
+      // handle *
+      break;
+
+    case '/':
+      // handle /
+      break;
+
+    case 'C':
+      // handle C
+      break;
+
+    case '=':
+      prior += $('#displayoutput').val() * 1;
+      $('#displayoutput').val(prior);
+      prior = 0;
+      break;
+
+    case '+/-':
+      // handle +/-
+      break;
+
+    default:
+      var current =  $('#displayoutput').val();
+      $('#displayoutput').val(current += buttonValue);
   }
+}
+
+// set prior = to buttonValue
+  // multiply prior by 10
+  // multiply next button press by 10
+  //
--- spec/test.js
+++ spec/test.js
@@ -1,69 +1,213 @@
 /* global describe, it */
 
-;(function () {
+;(function(){
   'use strict';
-    
-  
-  beforeEach(function(){
-    $('#displayoutput').val("");
-  });
 
+  beforeEach(function(){
+    $('#displayoutput').val('');
+  });
 
   describe('Entering numbers', function(){
 
+    describe('displayoutput', function(){
+      it('should return the value of the display', function(){
+        $('#displayoutput').val(1.23);
+        assert.equal(displayOutput(), 1.23);
+
+        $('#displayoutput').val(2.34);
+        assert.equal(displayOutput(), 2.34);
+      })
+    })
+
+    describe('press', function(){
+      it('should display the number pressed', function(){
+        assert.equal(displayOutput(), '');
+        press(1);
+        assert.equal(displayOutput(), 1);
+      })
+    })
+
     describe('Entering the decimal number 7 . 8 9', function(){
       it('should display 7.89', function(){
-        press('7');
+        press(7);
         press('.');
-        press('8');
-        press('9');
-        it('should be 7.89', function () {
-          assert.equal(displayOutput(), "10.89");
-        });
+        press(8);
+        press(9);
+        assert.equal(displayOutput(), 7.89);
       });
     });
   });
 
   describe('Adding', function () {
     describe('7 . 8 9 + 1 + 2 =', function(){
-      press('7');
-      press('.');
-      press('8');
-      press('9');
-      press('+');
-      press('1');
-      press('+');
-      press('2');
-      press('=');
       it('should be 10.89', function () {
-        assert.equal(displayOutput(), "10.89");
+        press(7);
+        press('.');
+        press(8);
+        press(9);
+        press('+');
+        press(1);
+        press('+');
+        press(2);
+        press('=');
+        assert.equal(displayOutput(), '10.89');
       });
     });
     describe('7 . 8 9 + 1 . 0 0 + 2 =', function(){
-      press('7');
-      press('.');
-      press('8');
-      press('9');
-      press('+');
-      press('1');
-      press('.');
-      press('0');
-      press('0');
-      press('+');
-      press('2');
-      press('=');
       it('should be 10.89', function () {
-        assert.equal(displayOutput(), "10.89");
+        press(7);
+        press('.');
+        press(8);
+        press(9);
+        press('+');
+        press(1);
+        press('.');
+        press(0);
+        press(0);
+        press('+');
+        press(2);
+        press('=');
+        assert.equal(displayOutput(), '10.89');
       });
     });
     describe('1 + 1 =', function(){
       it('should be 2', function () {
-        assert.equal(false);
+        press(1);
+        press('+');
+        press(1);
+        press('=');
+        assert.equal(displayOutput(), '2');
       });
     });
+
+    describe('. 1 + . 2 =', function(){
+      it('should be 0.3', function () {
+        press('.');
+        press(1);
+        press('+');
+        press('.');
+        press(2);
+        press('=');
+        assert.equal(displayOutput(), '0.3');
+      });
+    });
+
+    describe('. 01 + . 02 =', function(){
+      it('should be 0.03', function () {
+        press('.');
+        press(0);
+        press(1);
+        press('+');
+        press('.');
+        press(0);
+        press(2);
+        press('=');
+        assert.equal(displayOutput(), '0.03');
+      });
+    });
+
     describe('. 9 + . 1 =', function(){
-      it('should be 1', function () {
-        assert.equal(false);
+      it('should be 1', function(){
+        press('.');
+        press(9);
+        press('+');
+        press('.');
+        press(1);
+        press('=');
+        assert.equal(displayOutput(), '1');
+      });
+    });
+  });
+   
+  
+  
+  describe('Subtracting', function () {
+    describe('7 . 8 9 - 1 - 2 =', function(){
+      it('should be 4.89', function () {
+        press(7);
+        press('.');
+        press(8);
+        press(9);
+        press('-');
+        press(1);
+        press('-');
+        press(2);
+        press('=');
+        assert.equal(displayOutput(), '4.89');
+      });
+    });
+    describe('7 . 8 9 - 1 . 0 0 - 2 =', function(){
+      it('should be 4.89', function () {
+        press(7);
+        press('.');
+        press(8);
+        press(9);
+        press('-');
+        press(1);
+        press('.');
+        press(0);
+        press(0);
+        press('-');
+        press(2);
+        press('=');
+        assert.equal(displayOutput(), '4.89');
+      });
+    });
+    describe('1 - 1 =', function(){
+      it('should be 0', function () {
+        press(1);
+        press('-');
+        press(1);
+        press('=');
+        assert.equal(displayOutput(), '0');
+      });
+    });
+
+    describe('. 2 - . 1 =', function(){
+      it('should be 0.1', function () {
+        press('.');
+        press(2);
+        press('-');
+        press('.');
+        press(1);
+        press('=');
+        assert.equal(displayOutput(), '0.1');
+      });
+    });
+
+    describe('. 02 - . 01 =', function(){
+      it('should be 0.01', function () {
+        press('.');
+        press(0);
+        press(2);
+        press('-');
+        press('.');
+        press(0);
+        press(1);
+        press('=');
+        assert.equal(displayOutput(), '0.01');
+      });
+    });
+
+    describe('1 . 1 - . 1 =', function(){
+      it('should be 1', function(){
+        press(1);
+        press('.');
+        press(1);
+        press('-');
+        press('.');
+        press(1);
+        press('=');
+        assert.equal(displayOutput(), '1');
+      });
+    });
+    describe('1 - 3 =', function(){
+      it('should be -2', function(){
+        press(1);
+        press('-');
+        press(2);
+        press('=');
+        assert.equal(displayOutput(), '-2');
       });
     });
   });
